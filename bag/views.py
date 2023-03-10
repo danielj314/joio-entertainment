62be85ce677ac0c681e5ea5217a0a2e012b4cef4
danielj314 <danieljones314@gmail.com>
2023-03-10 17:37:11 +0000

    Added form to Add to booking button on products page, which adds 1 to the quantity of the item to bag

--- bag/views.py
+++ bag/views.py
@@ -10,7 +10,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
-    quantity = int(request.POST.get('quantity'))
+    quantity = int(request.POST.get('quantity', 1))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
