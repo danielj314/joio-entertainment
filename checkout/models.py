5f71730643b1a41781057ba9f6b78e343c487311
danielj314 <danieljones314@gmail.com>
2023-03-14 00:30:39 +0000

    Created profile app

--- checkout/models.py
+++ checkout/models.py
@@ -5,10 +5,13 @@ from django.db.models import Sum
 from django.conf import settings
 
 from products.models import Product
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
     event_date = models.DateField(null=True, blank=False)
     event_time = models.TimeField(null=True, blank=False)
     event_location = models.CharField(max_length=80, null=True, blank=False)
