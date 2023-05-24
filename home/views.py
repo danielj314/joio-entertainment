c9848db9ccc9ccf786faff6c2dd67e9cacfed8c7
danielj314 <danieljones314@gmail.com>
2023-05-24 16:52:22 +0000

    Fix internal 500 error

--- home/views.py
+++ home/views.py
@@ -1,15 +1,6 @@
 from django.shortcuts import render
-from reviews.models import Review
-import random
 
 
 def index(request):
     """ A view to return the index page """
-    reviews = list(Review.objects.all())
-    random_reviews = random.sample(reviews, k=3)
-
-    context = {
-        "random_reviews": random_reviews,
-    }
-
-    return render(request, 'home/index.html', context)
+    return render(request, 'home/index.html')
