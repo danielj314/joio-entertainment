8c2cb27f0b68c3ee33dcbe84219a1b7ae17f2c7c
danielj314 <danieljones314@gmail.com>
2023-05-24 16:41:43 +0000

    Test commit to resolve issue with homepage on heroku

--- joio_entertainment/settings.py
+++ joio_entertainment/settings.py
@@ -122,6 +122,7 @@ WSGI_APPLICATION = 'joio_entertainment.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
